3ae5b9ea335c4d3264f4f5669ebac8542d3e97a4
Larry N <larryn35@gmail.com>
2020-10-19 17:06:53 -0700

    initial commit

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Larry Nguyen on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
